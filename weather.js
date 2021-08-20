eabbc3a9f102652a0a6a5c91db87874749d785bf
Guruprasad <56597606+Guru06-code@users.noreply.github.com>
2021-08-20 22:46:08 +0530

    Add files via upload

---
+++ weather.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app. get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res){
+  const place = req.body.cityName;
+  const appid = "9cbd51eb9aae36d36d0f8a7dd7f920bf";
+  const units = "metric";
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + place + "&appid=" + appid + "&units=" + units;
+  https.get(url, function(response){
+    console.log(response.statusCode);
+
+    response.on("data", function(data){
+      const weatherData = JSON.parse(data);
+      const temp = weatherData.main.temp;
+      const weather = weatherData.weather[0].description;
+      const icon = weatherData.weather[0].icon;
+      const imgURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+      res.write("<h1>The temperature in " + place + " is : " + temp + " degree Celcius.</h1>");
+      res.write("<p>Weather description : " + weather + "</p>");
+      res.write("<img src=" + imgURL + ">");
+      res.send();
+    });
+  });
+});
+
+app.listen(3000, function() {
+  console.log("Server is running on port 3000");
+});
